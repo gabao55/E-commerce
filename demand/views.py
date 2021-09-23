c6105f508ba58e0f0a0521d0ed30c15c19b8ba0e
gabao55 <gabriel.s.rosin@gmail.com>
2021-09-23 08:36:16 -0300

    Creating views for demand

--- demand/views.py
+++ demand/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class Pay(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pay')
+
+class FinishDemand(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('FinishDemand')
+
+class Detail(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detail')
