404ae0a8aa09678a566455501316667dc5a832d4
gabao55 <gabriel.s.rosin@gmail.com>
2021-09-28 08:28:04 -0300

    Adding util functions

--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,16 @@
 def price_format(value):
     return f'$ {value:.2f}'
+
+def cart_total_amount(cart):
+    return sum([item['amount'] for item in cart.values()])
+
+def cart_totals(cart):
+    return sum(
+        [
+            item.get('quantitative_promotional_price')
+            if item.get('quantitative_promotional_price')
+            else item.get('quantitative_price')
+            for item 
+            in cart.valeus()
+        ]
+    )
