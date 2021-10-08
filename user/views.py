64a5ecf882bc1f0b4ac45427a506877fbb2c08aa
gabao55 <gabriel.s.rosin@gmail.com>
2021-10-08 09:56:08 -0300

    Modifying the views of user

--- user/views.py
+++ user/views.py
@@ -2,11 +2,41 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
+from . import models
+from . import forms
+
 # Create your views here.
 
-class Create(View):
+class ProfileBase(View):
+    template_name = 'user\create.html'
+    
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+        
+        if self.request.user.is_authenticated:
+            self.context = {
+                'userform': forms.UserForm(
+                    data=self.request.POST or None),
+                'profileform': forms.ProfileForm(
+                    data=self.request.POST or None)
+            }
+        else:
+            self.context = {
+                'userform': forms.UserForm(
+                    data=self.request.POST or None),
+                'profileform': forms.ProfileForm(
+                    data=self.request.POST or None)
+            }
+
+        self.render = render(self.request, self.template_name, self.context)
+
     def get(self, *args, **kwargs):
-        return HttpResponse('Create')
+        return self.render
+
+
+class Create(ProfileBase):
+    def post(self, *args, **kwargs):
+        return self.render
 
 class Update(View):
     def get(self, *args, **kwargs):
