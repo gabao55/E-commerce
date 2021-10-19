18a8125efd9abe4046f577c07964b3e811dc7f98
gabao55 <gabriel.s.rosin@gmail.com>
2021-10-19 14:08:10 -0300

    Modifying url path

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = 'user'
 
+#TODO: Create different urls for
 urlpatterns = [
     path('', views.Create.as_view(), name='create'),
-    path('update/', views.Update.as_view(), name='update'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
 ]
