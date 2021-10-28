9b1ad15d77f7a8e004be5f3412f8ac6a38adced5
gabao55 <gabriel.s.rosin@gmail.com>
2021-10-28 09:48:14 -0300

    Adding an url for the payment method session

--- demand/urls.py
+++ demand/urls.py
@@ -5,6 +5,7 @@ app_name = 'demand'
 
 urlpatterns = [
     path('', views.Pay.as_view(), name='pay'),
+    path('list/', views.List.as_view(), name='list'),
     path('savedemand/', views.SaveDemand.as_view(), name='savedemand'),
     path('detail/', views.Detail.as_view(), name='detail'),
 ]
