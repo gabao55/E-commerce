631df725a60953f49827654f46c658789477a353
gabao55 <gabriel.s.rosin@gmail.com>
2021-11-02 19:03:01 -0300

    Modifying reset_password function

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,8 @@
+from django.core.mail import send_mail
+import string
+import random
+from django.conf import settings
+
 def price_format(value):
     return f'$ {value:.2f}'
 
@@ -14,3 +19,27 @@ def cart_totals(cart):
             in cart.values()
         ]
     )
+
+def reset_password(email, name, username):
+    characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+    random.shuffle(characters)
+    password = []
+    for i in range(10):
+        password.append(random.choice(characters))
+
+    random.shuffle(password)
+
+    password = "".join(password)
+
+    subject = 'E-commerce - Reset your password'
+    #TODO: Fazer com que a mensagem seja enviada corretamente pro email
+    message = f"Hello {str(name)},\n\nWe've received your request for reseting your password." \
+    f"We provided you a temporary password, you can check you informations bellow:\n\n" \
+    f"Username: {username}\nTemporary password: {password}\n\n" \
+    f"To continue, click on the following link:\nhttp://127.0.0.1:8000/user/change_password/" \
+    f"\n\nThank you!"
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message, email_from, recipient_list)
+
+    return 
