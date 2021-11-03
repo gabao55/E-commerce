7478969f6d61c63696587b75dab0cd8cb8695581
gabao55 <gabriel.s.rosin@gmail.com>
2021-11-03 09:54:37 -0300

    Adding a forgot my password session

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'user'
 
-#TODO: Create different urls for "forgot my password session"
 urlpatterns = [
     path('', views.Create.as_view(), name='create'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
+    path('reset-password/', views.ForgotPassword.as_view(), name='password_reset'),
+    path('change-password/', views.ChangePassword.as_view(), name='change_password'),
 ]
