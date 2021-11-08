698b42104fe93861dfd52eed8a5737067183f857
gabao55 <gabriel.s.rosin@gmail.com>
2021-11-07 21:09:00 -0300

    Removing savedemand url

--- demand/urls.py
+++ demand/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'demand'
 
 urlpatterns = [
-    path('', views.Pay.as_view(), name='pay'),
+    path('pay/<int:pk>', views.Pay.as_view(), name='pay'),
     path('list/', views.List.as_view(), name='list'),
     path('savedemand/', views.SaveDemand.as_view(), name='savedemand'),
-    path('detail/', views.Detail.as_view(), name='detail'),
+    path('detail/<int:pk>', views.Detail.as_view(), name='detail'),
 ]
